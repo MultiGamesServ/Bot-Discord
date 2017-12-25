d004a8100267582c42b720dafc835b6ce76c5896
NathanDemoineret <34844580+NathanDemoineret@users.noreply.github.com>
2017-12-26 00:53:05 +0100

    Delete guildsMemberAdd.js

--- events/guildsMemberAdd.js
+++
@@ -1,6 +0,0 @@
-module.exports = member => {
-  let guild = member.guild;
-    let RoleToAdd = guild.roles.find("name", "Joueur");
-client.on("guildMemberAdd", member => {
-  member.addRole(RoleToAdd);
-  });
