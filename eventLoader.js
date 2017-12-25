b1f70c9ba7f5050db5e505e231d09a831716c54e
NathanDemoineret <34844580+NathanDemoineret@users.noreply.github.com>
2017-12-26 00:56:03 +0100

    Delete eventLoader.js

--- eventLoader.js
+++
@@ -1,9 +0,0 @@
-const reqEvent = (event) => require(`../events/${event}`);
-module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
-  client.on('reconnecting', () => reqEvent('reconnecting')(client));
-  client.on('disconnect', () => reqEvent('disconnect')(client));
-  client.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
-  client.on('guildBanAdd', reqEvent('guildBanAdd'));
-  client.on('guildBanRemove', reqEvent('guildBanRemove'));
-};
